64a2125bf320819130e16eca44237928ba98a2e0
MPSCampos <32508878+MPSCampos@users.noreply.github.com>
2025-01-16 15:07:36 +0000

    Delete shopping-list directory

--- shopping-list/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.{js,jsx}'],
-    testEnvironment: 'jsdom',
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-    },
-  };
